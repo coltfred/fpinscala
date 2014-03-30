b98d23dfa78cd398c1b474c9cb55c2a3b5ad31df
Colt Frederickson <coltfred@gmail.com>
2014-03-30 14:35:27 -0600

    Chapter 4

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -3,13 +3,25 @@ package fpinscala.errorhandling
 import scala.{ Option => _, Either => _, _ } // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = sys.error("todo")
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case l @ Left(_) => l
+  }
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case l @ Left(_) => l
+  }
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case r => r
+  }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = for {
+    a <- this
+    bb <- b
+  } yield f(a, bb)
 }
 case class Left[+E](get: E) extends Either[E, Nothing]
 case class Right[+A](get: A) extends Either[Nothing, A]
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -49,6 +49,8 @@ case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  def apply[A](a: A): Option[A] = if (a == null) None else Some(a)
+
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // The `val x: Int = ...` declares `x` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try {
@@ -69,9 +71,16 @@ object Option {
     else Some(xs.sum / xs.length)
   def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = for {
+    aa <- a
+    bb <- b
+  } yield f(aa, bb)
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity)
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a.foldRight(Option(List[B]())) { (item, acc) =>
+      map2(f(item), acc)(_ :: _)
+    }
+  }
 }
